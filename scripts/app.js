169a28ec18592335ba5f02cdeedafcb954a798ba
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-22 15:22:00 +0800

    Add example with show multidata

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ const app = createApp({
     data() {
         return {
             message: 'Hello Vue!',
+            secondMessage: `Hello Vue, I'm The Second Message`,
+            thirdMessage: `Hello Vue, I'm The Third Message`,
             messageFormatted: `<b>Hello</b> <i>Vue!</i> <u>I'm Here</u>`,
         }
     }
