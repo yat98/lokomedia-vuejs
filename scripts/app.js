15939adcec670155bb7ce26cf6b589b052406093
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-22 15:28:53 +0800

    Reset project

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,7 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
-            message: 'Hello Vue!',
-            secondMessage: `Hello Vue, I'm The Second Message`,
-            thirdMessage: `Hello Vue, I'm The Third Message`,
-            messageFormatted: `<b>Hello</b> <i>Vue!</i> <u>I'm Here</u>`,
-            text: 'Hello this is text',
-            number: 100,
-            float: 10.3,
-            booleanTrue: true,
-            booleanFalse: false,
-            array: [1,'two',3.5]
+            
         }
     }
 })
