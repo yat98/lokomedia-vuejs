dc5239ddffb96ed2ffc2a65a008204874fc06c5a
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-22 17:05:57 +0800

    Add exploration in computed and methods

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,35 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
-            numberOne: 1,
-            numberTwo: 2,
-            numberAmount: null,
+            numberOne: 3,
+            numberTwo: 8,
+            sum: 0,
+            subtraction: 0,
+            multiple: 0,
+            distribute: 0,
         }
     },
+    computed: {
+        // sum() {
+        //     return this.numberOne + this.numberTwo
+        // },
+        // subtraction() {
+        //     return this.numberOne - this.numberTwo
+        // },
+        // multiple() {
+        //     return this.numberOne * this.numberTwo
+        // },
+        // distribute() {
+        //     return this.numberOne / this.numberTwo
+        // },
+    },
     methods: {
-        calculate() {
-            this.numberAmount = this.numberOne + this.numberTwo
-        }
+       calculate() {
+            this.sum = this.numberOne + this.numberTwo
+            this.subtraction = this.numberOne - this.numberTwo
+            this.multiple = this.numberOne * this.numberTwo
+            this.distribute = this.numberOne / this.numberTwo
+       }
     }
 })
 
