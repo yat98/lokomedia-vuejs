d929d314f8795f5d8ff9183af391822e928d88ec
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-22 15:25:16 +0800

    Add example directive rendering with another data type

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,12 @@ const app = createApp({
             secondMessage: `Hello Vue, I'm The Second Message`,
             thirdMessage: `Hello Vue, I'm The Third Message`,
             messageFormatted: `<b>Hello</b> <i>Vue!</i> <u>I'm Here</u>`,
+            text: 'Hello this is text',
+            number: 100,
+            float: 10.3,
+            booleanTrue: true,
+            booleanFalse: false,
+            array: [1,'two',3.5]
         }
     }
 })
