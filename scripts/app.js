1c65c5a632e03b5033140932d73ddbfa2765fd5f
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-23 16:45:27 +0800

    Add example binding style

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,8 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
-            image: 'assets/img/flower.webp'
+            image: 'assets/img/flower.webp',
+            imageWidth: '300px'
         }
     }
 }).mount('#app')
