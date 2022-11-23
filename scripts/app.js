e004b022a609eb44645c71778d19af9aa605f685
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-23 12:52:03 +0800

    Add example radio button

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,8 @@ const app = createApp({
     data() {
         return {
             message: '',
-            messageArea: ''
+            messageArea: '',
+            choice: ''
         }
     }
 }).mount('#app')
