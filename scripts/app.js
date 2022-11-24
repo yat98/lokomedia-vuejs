c0197f12a912136fbf98081bd4f9c1e707ef4434
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-24 20:24:58 +0800

    Add example canvas draw image

--- scripts/app.js
+++ scripts/app.js
@@ -6,17 +6,44 @@ const app = createApp({
             y: 10,
             length: 50,
             width: 50,
-            color: 'blue',
+            colorOne: 'red',
+            colorTwo: 'green',
+            colorThree: 'blue',
+            colorFour: 'orange',
         }
     },
     directives: {
         canvas: (el, binding, vnode) => {
             const canvas  = el.getContext('2d')
             const bindingInstance = binding.instance
-            canvas.fillStyle = bindingInstance.color
+            canvas.fillStyle = bindingInstance.colorOne
             canvas.fillRect(
                 bindingInstance.x,
                 bindingInstance.y,
+                bindingInstance.length,
+                bindingInstance.width
+            )
+
+            canvas.fillStyle = bindingInstance.colorTwo
+            canvas.fillRect(
+                bindingInstance.x + 55,
+                bindingInstance.y,
+                bindingInstance.length,
+                bindingInstance.width
+            )
+            
+            canvas.fillStyle = bindingInstance.colorThree
+            canvas.fillRect(
+                bindingInstance.x,
+                bindingInstance.y + 55,
+                bindingInstance.length,
+                bindingInstance.width
+            )
+
+            canvas.fillStyle = bindingInstance.colorFour
+            canvas.fillRect(
+                bindingInstance.x + 55,
+                bindingInstance.y + 55,
                 bindingInstance.length,
                 bindingInstance.width
             )
