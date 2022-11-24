382a612274a86dfc1a56b1184df591c14ae98cb3
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-25 01:05:35 +0800

    Add example transition group

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,15 @@ const app = createApp({
     data() {
         return {
             show: false,
+            numbers: []
+        }
+    },
+    methods: {
+        add(){
+            this.numbers.push(this.numbers.length + 1)
+        },
+        remove(){
+            this.numbers.splice((this.numbers.length - 1 ), 1)
         }
     }
 }).mount('#app')
