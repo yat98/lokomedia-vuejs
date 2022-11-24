34f9ce7691cb170bbab93bf0679870cb1e062ee6
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-24 16:48:31 +0800

    Add example multi element

--- scripts/app.js
+++ scripts/app.js
@@ -7,4 +7,15 @@ app.component('bold', {
     template: '<b><slot></slot></b>'
 })
 
+app.component('multi', {
+    template: `
+        <div>
+            <b>This is bold</b>
+            <br>
+            <i>This is italic</i>
+            <br>
+            <u>This is underline</u>
+        </div>`
+})
+
 app.mount('#app')
