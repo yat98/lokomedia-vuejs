f2390f06f8dcb045ac5c20054c267582bbbbf55b
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-24 20:22:23 +0800

    Add example canvas with dynamic data

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,25 @@
 const { createApp } = Vue
 const app = createApp({
+    data() {
+        return {
+            x: 10,
+            y: 10,
+            length: 50,
+            width: 50,
+            color: 'blue',
+        }
+    },
     directives: {
-        canvas: (el) => {
+        canvas: (el, binding, vnode) => {
             const canvas  = el.getContext('2d')
-            canvas.fillRect(10,10,50,50)
+            const bindingInstance = binding.instance
+            canvas.fillStyle = bindingInstance.color
+            canvas.fillRect(
+                bindingInstance.x,
+                bindingInstance.y,
+                bindingInstance.length,
+                bindingInstance.width
+            )
         }
     }
 }).mount('#app')
