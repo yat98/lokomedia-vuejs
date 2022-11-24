08b3f3c196a7eae39a232530748a0085691ca136
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-24 20:33:33 +0800

    Add example canvas draw another image

--- scripts/app.js
+++ scripts/app.js
@@ -16,37 +16,67 @@ const app = createApp({
         canvas: (el, binding, vnode) => {
             const canvas  = el.getContext('2d')
             const bindingInstance = binding.instance
-            canvas.fillStyle = bindingInstance.colorOne
-            canvas.fillRect(
-                bindingInstance.x,
-                bindingInstance.y,
-                bindingInstance.length,
-                bindingInstance.width
-            )
+            // canvas.fillStyle = bindingInstance.colorOne
+            // canvas.fillRect(
+            //     bindingInstance.x,
+            //     bindingInstance.y,
+            //     bindingInstance.length,
+            //     bindingInstance.width
+            // )
 
-            canvas.fillStyle = bindingInstance.colorTwo
-            canvas.fillRect(
-                bindingInstance.x + 55,
-                bindingInstance.y,
-                bindingInstance.length,
-                bindingInstance.width
-            )
+            // canvas.fillStyle = bindingInstance.colorTwo
+            // canvas.fillRect(
+            //     bindingInstance.x + 55,
+            //     bindingInstance.y,
+            //     bindingInstance.length,
+            //     bindingInstance.width
+            // )
             
-            canvas.fillStyle = bindingInstance.colorThree
-            canvas.fillRect(
-                bindingInstance.x,
-                bindingInstance.y + 55,
-                bindingInstance.length,
-                bindingInstance.width
-            )
+            // canvas.fillStyle = bindingInstance.colorThree
+            // canvas.fillRect(
+            //     bindingInstance.x,
+            //     bindingInstance.y + 55,
+            //     bindingInstance.length,
+            //     bindingInstance.width
+            // )
 
-            canvas.fillStyle = bindingInstance.colorFour
-            canvas.fillRect(
-                bindingInstance.x + 55,
-                bindingInstance.y + 55,
-                bindingInstance.length,
-                bindingInstance.width
-            )
+            // canvas.fillStyle = bindingInstance.colorFour
+            // canvas.fillRect(
+            //     bindingInstance.x + 55,
+            //     bindingInstance.y + 55,
+            //     bindingInstance.length,
+            //     bindingInstance.width
+            // )
+
+            // Make Line
+            canvas.beginPath()
+            canvas.moveTo(30,120)
+            canvas.lineTo(50,60)
+            canvas.lineTo(60,100)
+            canvas.lineTo(100,30)
+            canvas.lineTo(120,70)
+            canvas.lineTo(170,25)
+            canvas.strokeStyle = bindingInstance.colorOne
+            canvas.stroke()
+
+            // Make Circle
+            canvas.beginPath()
+            canvas.arc(170,95,50,0,Math.PI * 2,false)
+            canvas.strokeStyle = bindingInstance.colorTwo
+            canvas.stroke()
+
+            // Make Text
+            canvas.font = '20px arial'
+            canvas.fillStyle = bindingInstance.colorThree
+            canvas.strokeText('Example',200,50)
+            canvas.fillText('Example',200,100)
+            canvas.beginPath()
+
+            // Make Curve
+            canvas.moveTo(500,100)
+            canvas.bezierCurveTo(200,10,400,10,400,100)
+            canvas.strokeStyle = bindingInstance.colorFour
+            canvas.stroke()
         }
     }
 }).mount('#app')
