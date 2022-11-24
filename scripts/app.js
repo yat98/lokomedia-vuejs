ae4a4ceaad7aa94a498e34070e68a8cd74b01ea8
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-24 17:06:13 +0800

    Add example component, slot & array

--- scripts/app.js
+++ scripts/app.js
@@ -18,4 +18,28 @@ app.component('multi', {
         </div>`
 })
 
+app.component('student', {
+    template: '<li><slot></slot></li>'
+})
+
+app.component('register', {
+    data() {
+        return {
+            students: [
+                {name: 'Eha', grade: 'XII'},
+                {name: 'Sri', grade: 'XI'},
+                {name: 'Rizky', grade: 'X'},
+            ]
+        }
+    },
+    template: `
+        <div>
+            <ol>
+                <student v-for="student in students">
+                    {{ student.name }} &mdash; {{ student.grade }}
+                </student>
+            </ol>
+        </div>`
+})
+
 app.mount('#app')
