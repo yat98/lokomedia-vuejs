b893990afd76064b0ebe19ae2eada163745d8c56
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-24 08:21:47 +0800

    Add example add data in array"

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,14 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
+            vechile: '',
             vechiles: ['car', 'motorcyle', 'bicycle']
+        }
+    },
+    methods: {
+        addVechile() {
+            this.vechiles.push(this.vechile)
+            this.vechile = ''
         }
     }
 }).mount('#app')
